463c7a6fc68d3c0efe90d56a180194912fca16a2
tomokazukatayama <32425706+tomokazukatayama@users.noreply.github.com>
2020-01-31 01:41:23 +0900

    LightCube Automation From Unity Collab to Github by YayoiMizuha

--- autogit.ps1
+++ autogit.ps1
@@ -1,7 +1,8 @@
 Param(
-    [parameter(mandatory=$true)][String]$PathArg
+    [parameter(mandatory=$true)][String]$PathArg,
+    [parameter(mandatory=$true)][String]$User
 )
-cd $PathArg
+Set-Location $PathArg
 git add -A
-git commit -m "LightCube Automation From Unity Collab to Github"
+git commit -m "LightCube Automation From Unity Collab to Github by $User"
 git push origin master
